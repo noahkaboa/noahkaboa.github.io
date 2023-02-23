e0d2dd5cbae19d030e6966f706b5d7cae1f9c33c
noahkaboa <116446071+noahkaboa@users.noreply.github.com>
2023-02-23 09:23:17 -0500

    IDOR 1

--- doors/idor.js
+++ doors/idor.js
@@ -1,5 +1,39 @@
-const params = new URLSearchParams(window.location.search)
-const connectionString = 'mongodb+srv://anon:anon@doors.4ozbh4f.mongodb.net/test'
+const params = new URLSearchParams(window.location.search);
 if (params.has('door')){
-
+    document.getElementById("doorHeader").innerHTML = titleCase(params.get('door'));
+    if (params.get('door') == "eyeron"){
+        alert("You got the flag! flag{12345}");
+    }
 }
+
+
+function titleCase(str) {
+    str = str.toLowerCase()
+             .split(' ')
+             .map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+  });
+   return str.join(' '); 
+}
+
+// const {MongoClient} = require('mongodb');
+
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+// }
+
+// async function main() {
+//     const uri = 'mongodb+srv://anon:anon@doors.4ozbh4f.mongodb.net/?retryWrites=true&w=majority';
+//     const client = new MongoClient(uri);
+//     try {
+//         await client.connect();
+//         await listDatabases(client);
+//     } catch(e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+// main().catch(console.error)
